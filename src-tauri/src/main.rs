70b8ac4d9a116ffd099c7a1e0286eae78c5a2375
Falk Pauser <falk.pauser@gmail.com>
2022-01-24 20:02:49 +0100

    Add tray icon support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,20 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+
 fn main() {
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(quit)
+    .add_native_item(SystemTrayMenuItem::Separator)
+    .add_item(hide);
+
+  let tray = SystemTray::new().with_menu(tray_menu);
+
   tauri::Builder::default()
+    .system_tray(tray)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
